96acab527973fa128e92787645193d431438ab47
hangmeng1995 <1178041733@qq.com>
2025-12-29 10:18:37 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["18.56.91.76"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["18.56.91.66"];
+pub const RS_PUB_KEY: &str = "5queK+MGPubZKgEmbApjyLaUtbs7rbZIaOH61K0bnt8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
